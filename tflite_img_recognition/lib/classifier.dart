64314aff06681398a641b6ff940a1d3d8258584e
schwarzwald0906 <schwarzwald0906@gmail.com>
2023-07-19 17:51:00 +0900

    多値分類

--- tflite_img_recognition/lib/classifier.dart
+++ tflite_img_recognition/lib/classifier.dart
@@ -1,93 +1,130 @@
 import 'dart:math';
+
 import 'package:image/image.dart';
+import 'package:collection/collection.dart';
+import 'package:logger/logger.dart';
 import 'package:tflite_flutter/tflite_flutter.dart';
 import 'package:tflite_flutter_helper/tflite_flutter_helper.dart';
 
-class Classifier {
-  // 推論エンジン
-  late Interpreter _interpreter;
-  // 推論用のオプション
-  // 例えば推論に使うCPUのスレッド数やAndroid/iOS用の特殊なライブラリの使用などを指定できる
+abstract class Classifier {
+  late Interpreter interpreter;
   late InterpreterOptions _interpreterOptions;
 
-  // 入力画像サイズ
+  var logger = Logger();
+
   late List<int> _inputShape;
-  // 出力画像サイズ
   late List<int> _outputShape;
 
-  // 出力結果格納バッファ
+  late TensorImage _inputImage;
   late TensorBuffer _outputBuffer;
 
-  // 入力の型
   late TfLiteType _inputType;
-  // 出力の型
   late TfLiteType _outputType;
 
-  // 重みファイル名
-  late final String _modelName;
+  final String _labelsFileName = 'assets/labels.txt';
 
-  // 前処理に使用する正規化オプション
-  final NormalizeOp _preProcessNormalizeOp = NormalizeOp(0, 1);
+  final int _labelsLength = 1001;
 
-  /* コンストラクタ */
-  Classifier(this._modelName) {
+  late var _probabilityProcessor;
+
+  late List<String> labels;
+
+  String get modelName;
+
+  NormalizeOp get preProcessNormalizeOp;
+  NormalizeOp get postProcessNormalizeOp;
+
+  Classifier({int? numThreads}) {
     _interpreterOptions = InterpreterOptions();
-    _interpreterOptions.threads = 1;
+
+    if (numThreads != null) {
+      _interpreterOptions.threads = numThreads;
+    }
 
     loadModel();
+    loadLabels();
   }
 
-  /* モデルのロード */
   Future<void> loadModel() async {
     try {
-      _interpreter =
-          await Interpreter.fromAsset(_modelName, options: _interpreterOptions);
+      interpreter =
+          await Interpreter.fromAsset(modelName, options: _interpreterOptions);
+      print('Interpreter Created Successfully');
 
-      _inputShape = _interpreter.getInputTensor(0).shape;
-      _inputType = _interpreter.getInputTensor(0).type as TfLiteType;
-      _outputShape = _interpreter.getOutputTensor(0).shape;
-      _outputType = _interpreter.getOutputTensor(0).type as TfLiteType;
+      _inputShape = interpreter.getInputTensor(0).shape;
+      _outputShape = interpreter.getOutputTensor(0).shape;
+      _inputType = interpreter.getInputTensor(0).type;
+      _outputType = interpreter.getOutputTensor(0).type;
 
       _outputBuffer = TensorBuffer.createFixedSize(_outputShape, _outputType);
-      print('Successfully model file is loaded.');
+      _probabilityProcessor =
+          TensorProcessorBuilder().add(postProcessNormalizeOp).build();
     } catch (e) {
-      print('Something is happened during loading the models: $e');
+      print('Unable to create interpreter, Caught Exception: ${e.toString()}');
     }
   }
 
-  /* 画像の前処理 */
-  TensorImage preProcess(TensorImage inputImage) {
-    // クロップサイズの指定
-    // 入力画像の高さと幅のうち、小さい方が入力画像のクロップサイズとなる
-    int cropSize = min(inputImage.height, inputImage.width);
+  Future<void> loadLabels() async {
+    labels = await FileUtil.loadLabels(_labelsFileName);
+    if (labels.length == _labelsLength) {
+      print('Labels loaded successfully');
+    } else {
+      print('Unable to load labels');
+    }
+  }
 
-    // 画像の前処理を行う
+  TensorImage _preProcess() {
+    int cropSize = min(_inputImage.height, _inputImage.width);
     return ImageProcessorBuilder()
         .add(ResizeWithCropOrPadOp(cropSize, cropSize))
         .add(ResizeOp(
             _inputShape[1], _inputShape[2], ResizeMethod.NEAREST_NEIGHBOUR))
-        .add(_preProcessNormalizeOp)
+        .add(preProcessNormalizeOp)
         .build()
-        .process(inputImage);
+        .process(_inputImage);
   }
 
-  /* 推論処理 */
-  double predict(Image image) {
-    // 入力の型を使ってTensorImageを初期化
-    TensorImage inputImage = TensorImage(_inputType);
+  Category predict(Image image) {
+    final pres = DateTime.now().millisecondsSinceEpoch;
+    _inputImage = TensorImage(_inputType);
+    _inputImage.loadImage(image);
+    _inputImage = _preProcess();
+    final pre = DateTime.now().millisecondsSinceEpoch - pres;
 
-    // 画像をロード
-    inputImage.loadImage(image);
-    // inputImageに対して前処理を行う
-    inputImage = preProcess(inputImage);
+    print('Time to load image: $pre ms');
 
-    // 推論処理
-    _interpreter.run(inputImage.buffer, _outputBuffer.getBuffer());
-    return _outputBuffer.getDoubleList()[0];
+    final runs = DateTime.now().millisecondsSinceEpoch;
+    interpreter.run(_inputImage.buffer, _outputBuffer.getBuffer());
+    final run = DateTime.now().millisecondsSinceEpoch - runs;
+
+    print('Time to run inference: $run ms');
+
+    Map<String, double> labeledProb = TensorLabel.fromList(
+            labels, _probabilityProcessor.process(_outputBuffer))
+        .getMapWithFloatValue();
+    final pred = getTopProbability(labeledProb);
+
+    return Category(pred.key, pred.value);
   }
 
-  /* 推論エンジンのDestroy */
   void close() {
-    _interpreter.close();
+    interpreter.close();
+  }
+}
+
+MapEntry<String, double> getTopProbability(Map<String, double> labeledProb) {
+  var pq = PriorityQueue<MapEntry<String, double>>(compare);
+  pq.addAll(labeledProb.entries);
+
+  return pq.first;
+}
+
+int compare(MapEntry<String, double> e1, MapEntry<String, double> e2) {
+  if (e1.value > e2.value) {
+    return -1;
+  } else if (e1.value == e2.value) {
+    return 0;
+  } else {
+    return 1;
   }
 }
